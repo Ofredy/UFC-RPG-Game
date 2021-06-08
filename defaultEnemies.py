d17965df745dea3f32744a8b9cebf413fc4d38dd
Fredy Orellana <82422969+Ofredy@users.noreply.github.com>
2021-06-07 23:25:24 -0500

    Add files via upload

---
+++ defaultEnemies.py
@@ -0,0 +1,12 @@
+# Imports all classes from characters.py
+from chracters import *
+
+# need to add png image for each of these characters
+enemy1 = Warrior("Alexander The Great", 50, "Axe", 13, "Sword", 8, "Stamina", 35)
+enemy2 = Chemist("Robert Boyle", 60, "Flame Thrower", 12, "Bioweapon", 10)
+enemy3 = Wizard("Voldermont", 70, "Ligthing Spell", 14, "Poison Attack", 10, "Mana", 45)
+enemy4 = Alien("King Vito Supreme Leader of the Algol Solar System", 80, "Particle Gun", 20, "Laser Attack", 18, "No stamin", 0, "Gravity Gun", 30)
+
+classList = ["Chemist", "Warrior", "Wizard"]
+warriorList = ["Axe", 15, "Sword", 10, "Bow and Arrow", 6]
+wizardList = ["Poison Spell", 10, "Ligthing Spell", 14, "Healing Power", 9]
